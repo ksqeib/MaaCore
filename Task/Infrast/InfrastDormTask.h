10ae8d994f1782c0db1e7b69a2eaf4af3eee7550
MistEO <mistereo@hotmail.com>
2022-12-02 00:22:47 +0800

    chore: breaking rename

---
+++ Task/Infrast/InfrastDormTask.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "InfrastAbstractTask.h"
+
+namespace asst
+{
+    class InfrastDormTask final : public InfrastAbstractTask
+    {
+    public:
+        using InfrastAbstractTask::InfrastAbstractTask;
+        virtual ~InfrastDormTask() override = default;
+
+        virtual size_t max_num_of_opers() const noexcept override { return 5ULL; }
+
+        InfrastDormTask& set_notstationed_enabled(bool dorm_notstationed_enabled) noexcept;
+        InfrastDormTask& set_trust_enabled(bool m_drom_trust_enabled) noexcept;
+
+    private:
+        virtual bool _run() override;
+        // virtual bool click_confirm_button() override;
+
+        bool opers_choose();
+        bool click_order_by_mood();
+
+        bool m_dorm_notstationed_enabled = false; // 设置是否启用未进驻筛选
+        bool m_drom_trust_enabled = true;         // 设置是否启用蹭信赖
+
+        int m_cur_dorm_index = 0;
+        int m_max_num_of_dorm = 4;
+
+        enum class NextStep
+        {
+            Rest,
+            RestDone,
+            Trust,
+            Fill,
+            AllDone,
+        };
+        NextStep m_next_step = NextStep::Rest;
+        bool m_if_filter_notstationed_haspressed = false;
+    };
+}
